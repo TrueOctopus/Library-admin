f0c9c6af757fd60626a35301a5329fe387895a9c
TrueOctopus <18368876370@163.com>
2022-03-16 15:11:32 +0800

    first commit

---
+++ src/settings.js
@@ -0,0 +1,23 @@
+/**
+ * @Author: 郑钊宇
+ * @Date: 2022-03-16 08:44:06
+ * @LastEditTime: 2022-03-16 14:42:12
+ * @LastEditors: 郑钊宇
+ * @Description:
+ */
+module.exports = {
+
+  title: 'Vue Admin Template',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
