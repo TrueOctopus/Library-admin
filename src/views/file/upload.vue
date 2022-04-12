712f8443d7d1ac642e97f5191cf8dc33ad4cae1c
TrueOctopus <18368876370@163.com>
2022-04-12 19:09:44 +0800

    upload picture

--- src/views/file/upload.vue
+++ src/views/file/upload.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: 郑钊宇
  * @Date: 2022-04-12 09:22:29
- * @LastEditTime: 2022-04-12 09:35:17
+ * @LastEditTime: 2022-04-12 19:08:59
  * @LastEditors: 郑钊宇
  * @Description:
 -->
@@ -9,30 +9,45 @@
   <div class="app-container">
     <el-upload
       class="upload-demo"
-      action="https://jsonplaceholder.typicode.com/posts/"
-      :on-preview="handlePreview"
+      action="http://localhost:8081/fileUpload/uploadPictureAction"
+      :headers="{'token': $store.getters.token}"
+      :on-success="handleImageSuccess"
       :on-remove="handleRemove"
       :file-list="fileList"
       list-type="picture"
+      name="oneFile"
     >
-      <el-button type="primary">点击上传</el-button>
+      <el-button v-waves class="filter-item" type="primary" icon="el-icon-upload">图片上传</el-button>
       <div slot="tip" class="el-upload__tip">只能上传jpg/png/gif文件</div>
     </el-upload>
-    <el-button v-waves class="filter-item" type="primary" icon="el-icon-upload" @click="uploadPic">
-      图片上传
-    </el-button>
-    <el-button v-waves class="filter-item" type="primary" icon="el-icon-upload" @click="uploadFile">
+
+    <el-button v-waves class="filter-item" type="primary" icon="el-icon-upload">
       文件上传
     </el-button>
-    <el-button v-waves class="filter-item" type="primary" icon="el-icon-upload" @click="uploadVid">
+    <el-button v-waves class="filter-item" type="primary" icon="el-icon-upload">
       视频上传
     </el-button>
   </div>
 </template>
 
 <script>
-export default {
+import { deleteFileByUuid } from '@/api/file'
 
+export default {
+  data() {
+    return {
+      fileList: []
+    }
+  },
+  methods: {
+    handleRemove(file, fileList) {
+      deleteFileByUuid(file.response.data.newFileName)
+      console.log(file, fileList)
+    },
+    handleImageSuccess(file) {
+      console.log(file)
+    }
+  }
 }
 </script>
 
