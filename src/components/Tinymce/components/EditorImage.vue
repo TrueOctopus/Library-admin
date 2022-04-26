ef57cd8dfced85e115b8b90edddd45f977df2993
TrueOctopus <18368876370@163.com>
2022-04-26 16:47:46 +0800

    修改样式及注意事项

--- src/components/Tinymce/components/EditorImage.vue
+++ src/components/Tinymce/components/EditorImage.vue
@@ -18,14 +18,14 @@
         list-type="picture-card"
       >
         <el-button size="small" type="primary">
-          Click upload
+          点击上传
         </el-button>
       </el-upload>
       <el-button @click="dialogVisible = false">
-        Cancel
+        取消
       </el-button>
       <el-button type="primary" @click="handleSubmit">
-        Confirm
+        确认
       </el-button>
     </el-dialog>
   </div>
--- src/views/publicity/components/Warning.vue
+++ src/views/publicity/components/Warning.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: 郑钊宇
  * @Date: 2022-04-01 10:39:44
- * @LastEditTime: 2022-04-26 15:07:24
+ * @LastEditTime: 2022-04-26 16:46:54
  * @LastEditors: 郑钊宇
  * @Description:
 -->
@@ -9,7 +9,18 @@
   <aside>
     注意事项：<br>
     ① 底部文章预览中的图片可能与实际会有差异，主要是图片的垂直居中问题，尽量避免同列图片的高度不一致；<br>
-    ② 此处文章图片为必传项，为书籍的封面图，尽量选择白边不多的图片以免影响观感。
+    ② 此处文章图片为必传项，为书籍的封面图，尽量选择白边不多的图片以免影响观感；<br>
+    ③ 在文章中添加图片可以使用右上角的图片上传，如果想引用已经上传的图片，可选择 插入>图片 地址处填写对应的链接，本系统图片地址为：{{ getPic }} + 新文件名。<br>
   </aside>
 </template>
-
+<script>
+import { getPic } from '@/api/file'
+export default {
+  name: 'Warning',
+  data() {
+    return {
+      getPic
+    }
+  }
+}
+</script>
